15eaf5f1dd9587eb0dacfc3274690bffff9ee150
Alex Erdei <mralexerdei@yahoo.co.uk>
2024-12-24 18:14:29 +0000

    Add error handling to file upload

--- components/home.js
+++ components/home.js
@@ -1,6 +1,7 @@
 import { upload } from "../backend/backend.js";
 import { importTemp } from "../helper.js";
 import { getUser } from "../index.js";
+import { showError, removeError } from "./register.js";
 
 let userLoggedIn;
 const WELCOME_TEXT_NO_USER = `Please login or
@@ -9,8 +10,17 @@ register if you do not have an account yet`;
 async function submit(event) {
 	event.preventDefault();
 	const form = event.target;
+	const submitBtn = form.querySelector("button[type = 'submit']");
+	submitBtn.setAttribute("disabled", "");
 	const json = await upload(form.file.files[0], userLoggedIn.token);
-	console.log(json);
+	removeError(form);
+	if (json.error) {
+		console.error(json.error);
+		showError(form, json.error);
+	} else {
+		submitBtn.removeAttribute("disabled");
+		console.log(json);
+	}
 }
 
 export function initHome() {
